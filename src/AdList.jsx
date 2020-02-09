fd5f31fd3b2b4d1d7a44e800619dc566d6254145
Aisha Gregg <aishagregg@gmail.com>
2020-02-09 19:57:59 +0100

    create adlist component

---
+++ src/AdList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./AdList.module.css";
+
+export function AdList({ ads }) {
+  return (
+    <div className={styles.border}>
+      {ads.map(ad => (
+        <section className={styles.ad} key={ad._id}>
+          <h2 className={styles.header2}>{ad.name}</h2>
+          <p className={styles.adinfo}>{ad.description}</p>
+          <p className={styles.adprice}>{ad.price}</p>
+          <img className={styles.adimage} src={ad.image} />
+          <p className={styles.tags}>
+            {ad.tags.map(tag => (
+              <span key={ad._id + tag}>{tag}</span>
+            ))}
+          </p>
+        </section>
+      ))}
+    </div>
+  );
+}
